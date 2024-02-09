119e207a95fa39638d06ac021333c92bbe14cefa
stephbertram <154558487+stephbertram@users.noreply.github.com>
2024-02-09 10:27:10 -0800

    Creation of All Poke List (#3)
    * begin initial list setup
    
    * fix error with list and add event listener

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const pokeAPI = 'https://pokeapi.co/api/v2/'
 const h1 = document.querySelector('h1')
 const selector = document.querySelector('#selector')
+const results = document.querySelector('#pokemon-list')
 // const howTo = document.querySelector('#how-to')
 
 
@@ -29,11 +30,29 @@ const getPokemon = () => {
             throw res.statusText
         })  
         .then(allPokeList => {
-            console.log(allPokeList.results)
+            allPokeList.results.forEach(result => displayAllPokemon(result))
         //    {debugger}
         })
         .catch(err => console.error(err))
 }
+
+//Display 
+
+const displayAllPokemon = (pokeListObj) => {
+    console.log(pokeListObj)
+    const li = document.createElement('li')
+    li.innerText = pokeListObj.name
+    results.appendChild(li)
+    li.addEventListener('click', e => handleClick(e))
+}
+
+
+// Event Handlers
+
+const handleClick = e => {
+    console.log(e.target)
+}
+
 
 // ! Start app logic on load
 const loadStuff = () => {
