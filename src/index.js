abba23aeeea7ecc1bfb4a6c4998665a70a13ee5e
pwangy <blipsandclicks@gmail.com>
2024-02-09 15:53:53 -0800

    start of displayProfile function

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@
 const pokeAPI = 'https://pokeapi.co/api/v2/'
 const h1 = document.querySelector('h1')
 const selector = document.querySelector('#selector')
-const results = document.querySelector('#pokemon-list')
-const profile = document.querySelector('#profile')
-// const howTo = document.querySelector('#how-to')
+const resultsList = document.querySelector('#pokemon-list')
 
+const profile = document.querySelector('#profile')
+const profileWrapper = document.createElement('article')
+const img = document.createElement('img')
+const name = document.createElement('h3')
 
 const teamArray = []
-
+let currentPoke = ''
 
 // ! Ref for filtering
 // search field /pokemon
 
 // Type endpoint GET https://pokeapi.co/api/v2/type/{id or name}/
 // "generation": "https://pokeapi.co/api/v2/generation/"
-
-
-
-//! Display pokemon profile
-
 
 // ! Fetch Data
 const getPokemon = () => {
@@ -31,27 +28,66 @@ const getPokemon = () => {
             throw res.statusText
         })  
         .then(allPokeList => {
-            allPokeList.results.forEach(result => displayAllPokemon(result))
-        //    {debugger}
+            allPokeList.results.forEach(pokemon => displayAllPokemon(pokemon))
         })
         .catch(err => console.error(err))
 }
 
+const getSpecificPoke = (currentPoke) => {
+    // console.log(currentPoke)
+        fetch(currentPoke)
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+                throw res.statusText
+            })  
+            .then(pokeInfoObj => {
+                displayProfile(pokeInfoObj)
+            })
+            .catch(err => console.error(err))
+}
+
 //Display 
 const displayAllPokemon = (pokeListObj) => {
-    console.log(pokeListObj)
     const li = document.createElement('li')
     li.innerText = pokeListObj.name
-    results.appendChild(li)
-    li.addEventListener('click', e => handleClick(e))
+    li.id = pokeListObj.url
+    resultsList.appendChild(li)
+    li.addEventListener('click', e => handleClick(e, pokeListObj))
 }
-
 
 // Event Handlers
-const handleClick = e => {
-    console.log(e.target)
+const reset = () => {
+    profileWrapper.remove()
+    img.src = ''
+    img.alt = ''
+    name.innerText = ''
+} 
+
+const handleClick = (e, pokeListObj) => {
+    reset()
+    currentPoke = e.target.id //sets specific pokemon's url
+    return getSpecificPoke(currentPoke)
 }
 
+//! Display pokemon profile
+const displayProfile = (pokeInfoObj) => {
+    console.log(pokeInfoObj) // print array of poke info
+    
+    profileWrapper.id = 'profile-wrapper'
+    profileWrapper.name = pokeInfoObj.id
+    img.src = pokeInfoObj.sprites.other.dream_world.front_default
+    img.alt = pokeInfoObj.name
+    name.innerText = pokeInfoObj.name
+
+    profileWrapper.append(img, name)
+    profile.append(profileWrapper)
+    // ability, attacks
+    // stage?
+    // type
+    // strength
+}
 
 // ! Start app logic on load
 const loadStuff = () => {
