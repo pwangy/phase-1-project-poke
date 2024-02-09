243285c8e090b08d87c3406a0067cb184f406d49
Peggy <blipsandclicks@gmail.com>
2024-02-08 21:50:33 -0800

    mob programming (#2)
    * push all changes
    
    * include project brief and deliverable info
    
    * make initial fetch call, global variables declared

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
 // ! Declare Globals
+const pokeAPI = 'https://pokeapi.co/api/v2/'
+const h1 = document.querySelector('h1')
+const selector = document.querySelector('#selector')
+// const howTo = document.querySelector('#how-to')
 
+
+const teamArray = []
+
+
+// ! Ref for filtering
+// search field /pokemon
+
+// Type endpoint GET https://pokeapi.co/api/v2/type/{id or name}/
+// "generation": "https://pokeapi.co/api/v2/generation/"
+
+
+
+//! Display pokemon profile
 
 
 // ! Fetch Data
+const getPokemon = () => {
+    return fetch(`${pokeAPI}pokemon`) 
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            }
+            throw res.statusText
+        })  
+        .then(allPokeList => {
+            console.log(allPokeList.results)
+        //    {debugger}
+        })
+        .catch(err => console.error(err))
+}
 
+// ! Start app logic on load
+const loadStuff = () => {
+    getPokemon()
+}
+
+loadStuff()
